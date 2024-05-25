56a5cfa0df51eca788df36369f30d8779a484c3e
florent-le-maire <lemaireflorent9@gmail.com>
2024-05-25 14:19:48 +0200

    Add dashing system

---
+++ Scripts/Player/Dash.cs
@@ -0,0 +1,40 @@
+using Godot;
+
+namespace PedroBulletV2.Scripts.Player;
+
+public partial class Dash : Node2D
+{
+
+	[Export] private Timer _dashTimer;
+	[Export] private Timer _dashCooldown;
+
+	public bool IsDashing = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void StartDash(float duration)
+	{
+		_dashTimer.WaitTime = duration;
+		_dashTimer.Start();
+		GD.Print("Start"+IsDashing);
+		IsDashing = true;
+	}
+
+	public bool IsInCooldown()
+	{
+		return _dashCooldown.IsStopped();
+	}
+
+	void _on_dash_timer_timeout()
+	{
+		IsDashing = false;
+		_dashCooldown.Start();
+	}
+}
--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -8,19 +8,38 @@ public partial class Player : CharacterBody2D
     [Export] public AudioStreamPlayer2D DamageSound;
     public int PlayerCurrentHealth { get; set; } = 4;
 
-    [Export] public int Speed { get; set; } = 200;
+    [Export] private int _normalSpeed = 200;
+    public int Speed { get; set; }
+
+    [Export] private Dash _dash;
+    [Export] private int _dashSpeed = 1000;
+    [Export] private float _dashLength = 0.1f;
+
 
     [Signal]
     public delegate void HealthChangedEventHandler(int currentHealth);
 
     public override void _Ready()
     {
+        Speed = _normalSpeed;
         SetMaximunHealth();
     }
 
     public void GetInput()
     {
         Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+        if (Input.IsActionJustPressed("dash") && _dash.IsInCooldown())
+        {
+            _dash.StartDash(_dashLength);
+        }
+        if (_dash.IsDashing)
+        {
+            Speed = _dashSpeed;
+        }
+        else
+        {
+            Speed = _normalSpeed;
+        }
         Velocity = inputDirection * Speed;
     }
 
